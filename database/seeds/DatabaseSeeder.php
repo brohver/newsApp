cb7cf3a6c1acdbbf3de64664e8ad912b02499280
brohver <adeayo35@gmail.com>
2020-03-06 10:33:08 +0100

    faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,14 +21,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       // $this->call(UsersTableSeeder::class);
-      /* factory(User::class, 100)->create();
+      factory(User::class, 100)->create();
       factory(Category::class, 5)->create();
       factory(Tag::class, 20)->create();
       factory(Post::class, 50)->create();
       factory(Comment::class, 500)->create();
       factory(Image::class, 100)->create();
       factory(Video::class, 100)->create();
-      factory(Like::class, 2000)->create(); */
+      factory(Like::class, 2000)->create();
       factory(Retweet::class, 2000)->create();
     }
 }
